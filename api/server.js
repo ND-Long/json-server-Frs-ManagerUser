4afd977f9c3766c3d54ab0c2044aaa6d860da0f8
ND-Long <nguyncl622001@gmail.com>
2023-03-05 08:10:17 +0700

    upppp

---
+++ api/server.js
@@ -0,0 +1,19 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(8000, () => {
+    console.log(`JSON server running at http://localhost:8000`)
+})
+
+// Export the Server API
+module.exports = server
