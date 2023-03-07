1e853429df88a69d9138e28f41fd0c3083eb48d2
ND-Long <nguyncl622001@gmail.com>
2023-03-07 15:51:29 +0700

    update data quiz

--- api/server.js
+++ api/server.js
@@ -7,20 +7,22 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/*': '/$1',
+    '/api/v1/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 
-// router.render = (req, res) => {
-//     return res.status(200).json({
-//         message: "ok"
-//     })
-// }
+
+
 
 server.use(router)
 server.listen(8000, () => {
     console.log(`JSON server running at http://localhost:8000`)
 })
+
+
+
+
+
 
 // Export the Server API
 module.exports = server
